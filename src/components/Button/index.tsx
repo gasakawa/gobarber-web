7694d64b002e4349b79eebc3f09dc6777a02d4fb
Gabriel Asakawa <gabriel.asakawa@gmail.com>
2020-07-02 23:34:55 -0300

    refactor: isolate button and input into components

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import React, { ButtonHTMLAttributes } from 'react';
+
+import * as S from './styles';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button: React.FC<ButtonProps> = ({ children, ...rest }) => {
+  return (
+    <S.Container type="button" {...rest}>
+      {children}
+    </S.Container>
+  );
+};
+export default Button;
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #232129;
+  border-radius: 10px;
+  border: 2px solid #232129;
+  padding: 16px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  color: #666360;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  input {
+    color: #f4ede8;
+    background: transparent;
+    flex: 1;
+    border: 0;
+
+    &::placeholder {
+      color: #666360;
+    }
+  }
+  svg {
+    margin-right: 16px;
+    color: #666360;
+  }
+`;
