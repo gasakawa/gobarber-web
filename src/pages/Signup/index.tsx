cc7c557d3572ea9fefb8f39cd225b551d687cfc5
Gabriel Asakawa <gabriel.asakawa@gmail.com>
2020-07-03 00:26:20 -0300

    feat: add unform lib to forms

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Form } from '@unform/web';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 
 import Button from '../../components/Button';
@@ -8,12 +9,14 @@ import logo from '../../assets/logo.svg';
 import * as S from './styles';
 
 const Signup: React.FC = () => {
+  const handleSubmit = (data: any): void => {};
+
   return (
     <S.Container>
       <S.Background />
       <S.Content>
         <img src={logo} alt="GoBarber" />
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
           <Input type="text" name="name" id="name" placeholder="Nome" icon={FiUser} />
 
@@ -21,7 +24,7 @@ const Signup: React.FC = () => {
           <Input type="password" name="password" id="password" placeholder="Senha" icon={FiLock} />
 
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
         <a href="lll">
           <FiArrowLeft />
           Voltar para o login
